889c8d47f80ea36607a352c69a5faf97aee6825e
= <=>
2022-09-21 14:37:00 -0500

    cambios widget de conexion serial

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import sys
 from PyQt5 import QtWidgets as qtw
-from chart.chartWidget import SensorChart
+from DAWidget.DAWidget import DAWidget
 
 class MainWidget(qtw.QMainWindow):
 
     def __init__(self) -> None:
         super().__init__()
-        #self._serial = ArduinoSerial(baudrate=9600, timeout=0.5)
         #self.init_window()
-        self.setCentralWidget(SensorChart())
+        self.setCentralWidget(DAWidget())
         self.show()
 
     """ def init_window(self):
@@ -22,34 +21,6 @@ class MainWidget(qtw.QMainWindow):
         main_layout.addWidget(self.data_groupbox)
         self.setLayout(main_layout)
         
-    def _init_connect_group(self):
-        self.connection_layout = qtw.QVBoxLayout()
-
-        #para seleccionar el puerto COM
-        self.port_combo = qtw.QComboBox(self)
-        self.port_combo.setMinimumSize(100, 25)
-        self.port_combo.currentTextChanged.connect(self._change_port)
-        self.connection_layout.addWidget(self.port_combo)
-        self._refresh_ports()
-
-        #Boton para conectarse al puerto serie
-        self.connect_button = qtw.QPushButton('Conectar', self)
-        self.connect_button.setMinimumSize(100, 25)
-        self.connect_button.clicked.connect(self._connect)
-        self.connection_layout.addWidget(self.connect_button)
-
-         #Boton paraactualizar la lista de puertos series
-        self.update_button = qtw.QPushButton('Actualizar', self)
-        self.update_button.setMinimumSize(100, 25)
-        self.update_button.clicked.connect(self._refresh_ports)
-        self.connection_layout.addWidget(self.update_button)
-
-        self.connect_groupbox = qtw.QGroupBox('Conectar Puerto', self) 
-        self.connect_groupbox.setMinimumSize(100, 25)
-        self.connect_groupbox.setMaximumWidth(200)
-        self.connect_groupbox.setSizePolicy(qtw.QSizePolicy.Maximum, qtw.QSizePolicy.Fixed)
-        self.connect_groupbox.setLayout(self.connection_layout)
-
     def _init_data_group(self):
         self.data_layout = qtw.QVBoxLayout()
 
@@ -71,29 +42,7 @@ class MainWidget(qtw.QMainWindow):
         self.data_groupbox.setSizePolicy(qtw.QSizePolicy.Maximum, qtw.QSizePolicy.Fixed)
         self.data_groupbox.setLayout(self.data_layout)
 
-    def _refresh_ports(self):
-        ports = ArduinoSerial.port_list()
-        self.port_combo.addItems(ports)
-
-        if(len(ports) > 0):
-            self.port_combo.setCurrentIndex(0)
-            self._serial.port = ports[0]
-
-    def _connect(self):
-        if not self._serial.port is None:
-            try: 
-                if self._serial.test_connection():
-                    self.connect_button.setText('Desconectar')
-                    self.connect_button.clicked.connect(self._disconnect)
-                    self.port_combo.setEnabled(False)
-            except:
-                print(sys.exc_info()[0])
-        
-    def _disconnect(self):
-        pass
     
-    def _change_port(self, name: str):
-        self._serial.port = name
  """
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
